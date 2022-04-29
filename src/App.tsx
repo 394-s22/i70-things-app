4e535dee6684474fdd59143ca45b5a3ce798615e
J-Lee12 <joshualee2022.1@u.northwestern.edu>
2022-04-29 17:45:12 -0500

    added more libraries from mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import { AppBar } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Box, Fab } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import MenuIcon from '@mui/icons-material/Menu';
+import AddIcon from '@mui/icons-material/Add';
+import SearchIcon from '@mui/icons-material/Search';
+import MoreIcon from '@mui/icons-material/MoreVert';
 import './App.css';
+
+
+const StyledFab = styled(Fab)({
+  position: 'absolute',
+  zIndex: 1,
+  top: -30,
+  left: 0,
+  right: 0,
+  margin: '0 auto',
+});
 
 function App() {
   return (
     <div className="App">
-      <AppBar />
+      <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+        <Toolbar>
+          <IconButton color="inherit" aria-label="open drawer">
+            <MenuIcon />
+          </IconButton>
+          <StyledFab color="secondary" aria-label="add">
+            <AddIcon />
+          </StyledFab>
+          <Box sx={{ flexGrow: 1 }} />
+          <IconButton color="inherit">
+            <SearchIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <MoreIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
