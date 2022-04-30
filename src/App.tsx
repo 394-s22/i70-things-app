66cb6e6051684acc8462498764713fe2c82a815a
Joshua Hoffman <joshuahoffman2023@u.northwestern.edu>
2022-04-30 09:51:00 -0500

    add prettier & reformat

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import { theme } from "./utils/theme";
 
 function App() {
-	return (
-		<ThemeProvider theme={theme}>
-			<div className="App">
-				<Footer />
-			</div>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Footer />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
