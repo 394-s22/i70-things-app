27c0ced84b2145f2c798646ac1db3ae7c67dbd7d
Joshua Hoffman <joshuahoffman2023@u.northwestern.edu>
2022-04-29 22:08:34 -0500

    theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import './App.css';
-import Footer from './components/Footer';
+import "./App.css";
+import Footer from "./components/Footer";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import { theme } from "./utils/theme";
 
 function App() {
-  return (
-    <div className="App">
-      <Footer/>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<div className="App">
+				<Footer />
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,35 +4,34 @@ import MenuIcon from "@mui/icons-material/Menu";
 import AddIcon from "@mui/icons-material/Add";
 import SearchIcon from "@mui/icons-material/Search";
 import MoreIcon from "@mui/icons-material/MoreVert";
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
-import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
 const StyledFab = styled(Fab)({
-  position: "absolute",
-  zIndex: 1,
-  top: -30,
-  left: 0,
-  right: 0,
-  margin: "0 auto",
+	position: "absolute",
+	zIndex: 1,
+	top: -30,
+	left: 0,
+	right: 0,
+	margin: "0 auto",
 });
 
 function Footer() {
-  return (
-    <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }} component="footer">
-      <Toolbar>
-        <IconButton color="inherit" aria-label="open drawer">
-          <MapOutlinedIcon />
-        </IconButton>
-        <StyledFab color="error" aria-label="add">
-          <AddIcon />
-        </StyledFab>
-        <Box sx={{ flexGrow: 1 }} />
-        <IconButton color="inherit">
-          <FormatListBulletedIcon />
-        </IconButton>
-        
-      </Toolbar>
-    </AppBar>
-  );
+	return (
+		<AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }} component="footer">
+			<Toolbar>
+				<IconButton color="inherit" aria-label="open drawer">
+					<MapOutlinedIcon />
+				</IconButton>
+				<StyledFab color="secondary" aria-label="add">
+					<AddIcon />
+				</StyledFab>
+				<Box sx={{ flexGrow: 1 }} />
+				<IconButton color="inherit">
+					<FormatListBulletedIcon />
+				</IconButton>
+			</Toolbar>
+		</AppBar>
+	);
 }
 export default Footer;
