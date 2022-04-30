1717b08f457244727b394eee7a2189b5510001ca
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2022-04-29 19:30:04 -0500

    Added links to the pages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ import AddIcon from "@mui/icons-material/Add";
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 import { AppBar, Box, Fab, IconButton, styled, Toolbar } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const StyledFab = styled(Fab)({
   position: "absolute",
@@ -17,14 +18,18 @@ const Footer = () => {
     <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }} component="footer">
       <Toolbar>
         <IconButton color="inherit" aria-label="open drawer">
-          <MapOutlinedIcon />
+          <Link to="/" className="link">
+            <MapOutlinedIcon />
+          </Link>
         </IconButton>
         <StyledFab color="error" aria-label="add">
           <AddIcon />
         </StyledFab>
         <Box sx={{ flexGrow: 1 }} />
         <IconButton color="inherit">
-          <FormatListBulletedIcon />
+          <Link to="/feed" className="link">
+            <FormatListBulletedIcon />
+          </Link>
         </IconButton>
       </Toolbar>
     </AppBar>
