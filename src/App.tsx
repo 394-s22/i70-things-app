044cc8af764d121fc785d0c88b762c9b95c63414
Nikhil Aggarwal <nikhil0929@gmail.com>
2022-05-01 20:36:37 -0500

    added some map stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,24 @@ import { Route, Routes } from "react-router-dom";
 import FeedPage from "./routes/FeedPage";
 import MapPage from "./routes/MapPage";
 import Footer from "./components/Footer";
-import Header from './components/Header';
-
+import Header from "./components/Header";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import "./App.css";
 
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibmlraGlsMDkyOSIsImEiOiJjbDJvMWFuM3AxMmFtM2JzM2VwbmZhejZmIn0.LQpNYBoPUTZY4q7EpAGOdg";
+
 const App = () => {
-	return (
-		<div className="App">
-      <Header/>
-        <Routes>
-          <Route path="/" element={<MapPage />} />
-          <Route path="/feed" element={<FeedPage />} />
-        </Routes>
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<MapPage />} />
+        <Route path="/feed" element={<FeedPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- src/routes/MapPage.tsx
+++ src/routes/MapPage.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+//import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+const mapboxgl = require("mapbox-gl");
 
 const MapPage = () => {
-	return <>Map Page</>;
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoibmlraGlsMDkyOSIsImEiOiJjbDJvMWFuM3AxMmFtM2JzM2VwbmZhejZmIn0.LQpNYBoPUTZY4q7EpAGOdg";
+  const map = new mapboxgl.Map({
+    container: "map", // container ID
+    style: "mapbox://styles/mapbox/streets-v11", // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+  });
+  return <div id="map"></div>;
 };
 
 export default MapPage;
