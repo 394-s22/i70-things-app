71f8d425abfade3396b29900a9458d37ce2eced1
Nikhil Aggarwal <nikhil0929@gmail.com>
2022-05-02 23:28:32 -0500

    Added some styling to FeedCards

--- src/components/ReportFeedItem.tsx
+++ src/components/ReportFeedItem.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Box, Typography, Paper } from "@mui/material/";
 import { Report } from "../utils/types";
+import PushPinIcon from "@mui/icons-material/PushPin";
+import DirectionsIcon from "@mui/icons-material/Directions";
 
 interface ReportCardProps {
   report: Report;
@@ -8,21 +10,26 @@ interface ReportCardProps {
 // A reportFeedCard
 const ReportFeedCard = ({ report }: ReportCardProps) => {
   return (
-    <Box style={{
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "space-between",
-      marginBottom: "1rem",
-    }}>
-      <Box component="img" src="https://picsum.photos/600/430?id=828"
+    <Box
       style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginBottom: "1rem",
+      }}
+    >
+      <Box
+        component="img"
+        src="https://picsum.photos/600/430?id=828"
+        style={{
           borderRadius: "50%",
-          width:"100px",
+          width: "100px",
           height: "100px",
           backgroundColor: "red",
           marginRight: "1rem",
-        }}></Box>
+        }}
+      ></Box>
       <Paper
         elevation={3}
         style={{
@@ -32,10 +39,47 @@ const ReportFeedCard = ({ report }: ReportCardProps) => {
           padding: "1rem",
         }}
       >
-        <Typography>Description: {report.description}</Typography>
-        <Typography>{report.timestamp}</Typography>
-        <Typography>Mile Marker: {report.mileMarker}</Typography>
-        <Typography>{report.direction}</Typography>
+        <Typography
+          component="h2"
+          style={{
+            fontSize: "2rem",
+          }}
+        >
+          {report.description}
+        </Typography>
+        <Typography
+          style={{
+            color: "#3c6ffe",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <PushPinIcon
+            style={{
+              marginRight: "0.5rem",
+              fontSize: "1.25rem",
+            }}
+          />
+          Mile Marker {report.mileMarker}
+        </Typography>
+        <Typography
+          style={{
+            color: "#3c6ffe",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <DirectionsIcon
+            style={{
+              marginRight: "0.5rem",
+              fontSize: "1.25rem",
+            }}
+          />
+          {report.direction}
+        </Typography>
+        <Typography variant="caption">{"2 min ago (14:53)"}</Typography>
       </Paper>
     </Box>
   );
