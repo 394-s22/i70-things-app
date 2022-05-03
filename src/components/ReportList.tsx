1e285bbb8e57733a972ed245ba5811ad4cbd7d6f
MierafM <mierafmulat@gmail.com>
2022-05-02 20:42:47 -0500

    started components

---
+++ src/components/ReportList.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      
+    </Box>
+  );
+}
