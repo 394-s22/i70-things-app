50b64afa3b5773d16205c70f3329811af0ea4b14
Joshua Hoffman <joshuahoffman2023@u.northwestern.edu>
2022-05-04 16:38:17 -0500

    refactor to use airtable sdk

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,10 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import { theme } from "./utils/theme";
-import Airtable from "airtable";
 import useData from "./utils/useData";
 
 import "./App.css";
 import axios from "axios";
-
-new Airtable({ apiKey: "keylvCxPGVKWeoE4u" }).base("appXtIml8So8edpwF");
-
-axios.defaults.baseURL =
-  "https://api.airtable.com/v0/appXtIml8So8edpwF/reports"; //content type to send with all POST requests
-
-axios.defaults.headers.post["Content-Type"] = "application/json"; //authenticate to the base with the API key
-// @ts-ignore
-axios.defaults.headers["Authorization"] = "Bearer keylvCxPGVKWeoE4u";
 
 const App = () => {
   return (
--- src/utils/useData.ts
+++ src/utils/useData.ts
@@ -1,9 +1,8 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
-import { Report } from "./types";
+import { base } from "./airtable";
 
-export default function useData<T>(): [T | null] {
-  const [data, setData] = useState(null);
+export default function useFetchRecords<T>(): [T | null] {
+  const [data, setData] = useState<T | null>(null);
 
   useEffect(() => {
     async function onPageLoad() {
@@ -13,13 +12,19 @@ export default function useData<T>(): [T | null] {
   }, []);
 
   const getData = async () => {
-    return axios.get("/").then((res) => {
-      //@ts-ignore
-      const formattedResult = res.data.records.map((record) => {
-        return record.fields;
+    base("reports")
+      .select()
+      .eachPage(function page(records, fetchNextPage) {
+        const data = records.map((record) => ({
+          timestamp: record.get("timestamp"),
+          description: record.get("description"),
+          direction: record.get("direction"),
+          mileMarker: record.get("mileMarker"),
+        }));
+        // @ts-ignore
+        setData(data as T);
+        fetchNextPage();
       });
-      setData(formattedResult);
-    });
   };
   return [data];
 }
