f209de2715fab694d31eba9cdf5892e2e43a3639
Ben Miller <benmiller2023.2@u.northwestern.edu>
2022-05-11 20:37:07 -0500

    cdot data progress Ben, Nikhil, Josh^2

--- src/components/ReportFeed.tsx
+++ src/components/ReportFeed.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import * as React from "react";
+import { useState, useEffect } from "react";
 import useFetchReports from "../hooks/useFetchReports";
 import { Report } from "../utils/types";
 import ReportFeedItem from "./ReportFeedItem";
@@ -8,6 +8,12 @@ import useCDOTData from "../hooks/useCDOTData";
 const ReportFeed = () => {
   const { reports, loading } = useFetchReports();
   const { data, dataLoading } = useCDOTData();
+
+  const [allData, setAllData] = useState<Report[]|null>(null);
+  useEffect(() => {
+    //const reportedData = data.map((x) => new Report())
+    setAllData(reports.concat(data));
+  }, [reports, data]);
 
   if (loading) {
     return <Typography variant="h6">Loading...</Typography>;
