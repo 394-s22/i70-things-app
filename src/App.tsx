6c3f7fde080e0f050005abaa6107dfd1e0141444
Nikhil Aggarwal <nikhil0929@gmail.com>
2022-05-26 14:14:12 -0500

    Put the SetOrigin and SetDestination states in the parent component App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,26 @@ import MapPage from "./routes/MapPage";
 import { theme } from "./utils/theme";
 import { Box } from "@mui/material";
 import useCDOTData from "./hooks/useCDOTData";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const { data } = useCDOTData();
+  
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState(null);
+
+  const getOrg = () => {
+    return origin
+  }
+
+  const getDst = () => {
+    return destination
+  }
+
+  useEffect(() => {
+    console.log("origin: ", origin)
+    console.log("destination: ", destination)
+  }, [origin, destination])
 
   console.log(data);
 
@@ -19,8 +36,8 @@ const App = () => {
       <Box className="App" height="100%">
         <Header />
         <Routes>
-          <Route path="/" element={<MapPage />} />
-          <Route path="/feed" element={<FeedPage />} />
+          <Route path="/" element={<MapPage setOrigin={setOrigin} setDestination={setDestination} getDst={getDst} getOrg={getOrg}/>} />
+          <Route path="/feed" element={<FeedPage/>} />
         </Routes>
         <Footer />
       </Box>
--- src/routes/MapPage.tsx
+++ src/routes/MapPage.tsx
@@ -9,131 +9,124 @@ import getCoordinateData, { markerToCoords } from "../utils/getCoordinateData";
 // const mapboxgl = require("mapbox-gl");
 
 mapboxgl.accessToken =
-  "pk.eyJ1IjoibmlraGlsMDkyOSIsImEiOiJjbDJvMWFuM3AxMmFtM2JzM2VwbmZhejZmIn0.LQpNYBoPUTZY4q7EpAGOdg";
+    "pk.eyJ1IjoibmlraGlsMDkyOSIsImEiOiJjbDJvMWFuM3AxMmFtM2JzM2VwbmZhejZmIn0.LQpNYBoPUTZY4q7EpAGOdg";
 
-const MapPage = () => {
-  const mapContainer = useRef(null);
-  const mapRef = useRef(null);
-  const [lng, setLng] = useState(0);
-  const [lat, setLat] = useState(0);
-  const [zoom] = useState(5);
+const MapPage = ({ setOrigin, setDestination, getDst, getOrg }) => {
+    const mapContainer = useRef(null);
+    const mapRef = useRef(null);
+    const [lng, setLng] = useState(0);
+    const [lat, setLat] = useState(0);
+    const [zoom] = useState(5);
 
-  const { reports, loading } = useFetchReports();
+    const { reports, loading } = useFetchReports();
 
-  const successLocation = (position) => {
-    setLat(position.coords.latitude);
-    setLng(position.coords.longitude);
-  };
-
-  const errorLocation = () => {
-    setLat(0);
-    setLng(0);
-  };
-
-  const [mapLoaded, setMapLoaded] = useState(false);
-  const [origin, setOrigin] = useState(null);
-  const [destination, setDestination] = useState(null);
-
-  useEffect(() => {
-    // Removed to allow the map to use the lng and lat properties.
-    // if (map.current) return; // initialize map only once
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-
-    navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-      enableHighAccuracy: true,
-    });
-
-    const nav = new mapboxgl.NavigationControl();
-    map.addControl(nav, "top-right");
-
-    var directions = new MapboxDirections({
-      controls: {
-        profileSwitcher: false,
-      },
-      accessToken: mapboxgl.accessToken,
-      profile: "mapbox/driving",
-      interactive: false
-    });
-
-    directions.on("origin", (e) => {
-      syncCall(e.feature.geometry.coordinates, 'o')
-    });
-    
-
-    directions.on("destination", (e) => {
-      syncCall(e.feature.geometry.coordinates, 'd')
-      
-    });
-    function syncCall(location, type){
-      console.log("1 ", location);
-      if (type == 'o'){
-        setOrigin(location);
-      }else{
-        setDestination(location);
-      }
-      filterReport()
-    }
-    
-    function filterReport(){
-      console.log('origin ', origin, ' dest ', destination)
-      if (origin && destination) {
-
-        var farRight = Math.max(origin[1], destination[1]);
-        var farLeft = Math.min(origin[1], destination[1]);
-        console.log('limits', farRight, farLeft)
-        reports.filter( report => {
-          console.log('report', report.long, report.lat)
-          if (report.long < farRight & report.long > farLeft){
-            return true
-          }
-          return false
-        })
-      }
-    }
-
-    
-
-    map.addControl(directions, "top-left");
-    map.on("data", () => {
-      setMapLoaded(true);
-    });
-
-    mapRef.current = map;
-
-    return () => {
-      map.off("data");
+    const successLocation = (position) => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
     };
-  }, [lat, lng, zoom]);
 
-  useEffect(() => {
-    console.log(mapRef.current);
-    if (loading || !mapLoaded) {
-      return;
-    }
+    const errorLocation = () => {
+        setLat(0);
+        setLng(0);
+    };
 
-    reports.forEach((report) => {
-      if (report.lat && report.long) {
-        var popup = new mapboxgl.Popup().setText(report.description);
-        new mapboxgl.Marker()
-          .setLngLat({ lon: report.long, lat: report.lat })
-          .addTo(mapRef.current)
-          .setPopup(popup);
-      }
-    });
-  }, [reports, loading, mapLoaded]);
+    const [mapLoaded, setMapLoaded] = useState(false);
+    //   const [origin, setOrigin] = useState(null);
+    //   const [destination, setDestination] = useState(null);
 
-  return (
-    <Box
-      style={{ height: "calc(100vh-120px)" }}
-      height="calc(100vh - 128px)"
-      ref={mapContainer}
-    ></Box>
-  );
+    useEffect(() => {
+        // Removed to allow the map to use the lng and lat properties.
+        // if (map.current) return; // initialize map only once
+        const map = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: "mapbox://styles/mapbox/streets-v11",
+            center: [lng, lat],
+            zoom: zoom,
+        });
+
+        navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+            enableHighAccuracy: true,
+        });
+
+        const nav = new mapboxgl.NavigationControl();
+        map.addControl(nav, "top-right");
+
+        var directions = new MapboxDirections({
+            controls: {
+                profileSwitcher: false,
+            },
+            accessToken: mapboxgl.accessToken,
+            profile: "mapbox/driving",
+            interactive: false
+        });
+
+        directions.on("origin", (e) => {
+            setOrigin(e.feature.geometry.coordinates)
+            filterReport()
+        });
+
+
+        directions.on("destination", (e) => {
+            setDestination(e.feature.geometry.coordinates)
+            filterReport()
+        });
+
+        function filterReport() {
+            let org = getOrg()
+            let dst = getDst()
+            console.log('origin ', org, ' dest ', destination)
+            if (org && dst) {
+                var farRight = Math.max(org[1], dst[1]);
+                var farLeft = Math.min(org[1], dst[1]);
+                console.log('limits', farRight, farLeft)
+                reports.filter(report => {
+                    console.log('report', report.long, report.lat)
+                    if (report.long < farRight & report.long > farLeft) {
+                        return true
+                    }
+                    return false
+                })
+            }
+        }
+
+
+
+        map.addControl(directions, "top-left");
+        map.on("data", () => {
+            setMapLoaded(true);
+        });
+
+        mapRef.current = map;
+
+        return () => {
+            map.off("data");
+        };
+    }, [lat, lng, zoom]);
+
+    useEffect(() => {
+        console.log(mapRef.current);
+        if (loading || !mapLoaded) {
+            return;
+        }
+
+        reports.forEach((report) => {
+            if (report.lat && report.long) {
+                var popup = new mapboxgl.Popup().setText(report.description);
+                new mapboxgl.Marker()
+                    .setLngLat({ lon: report.long, lat: report.lat })
+                    .addTo(mapRef.current)
+                    .setPopup(popup);
+            }
+        });
+    }, [reports, loading, mapLoaded]);
+
+    return (
+        <Box
+            style={{ height: "calc(100vh-120px)" }}
+            height="calc(100vh - 128px)"
+            ref={mapContainer}
+        ></Box>
+    );
 };
 
 export default MapPage;
