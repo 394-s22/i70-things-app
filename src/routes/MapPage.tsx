bae091856298e872236029c0985b43c7772f4df5
Fardeem Munir <munirfardeem@gmail.com>
2022-05-25 21:08:01 -0500

    Make map better
    Co-authored by: Javier <javiercuadra2020@u.northwestern.edu>

--- src/routes/MapPage.tsx
+++ src/routes/MapPage.tsx
@@ -15,19 +15,8 @@ const MapPage = () => {
   const mapRef = useRef(null);
   const [lng, setLng] = useState(0);
   const [lat, setLat] = useState(0);
-  const [zoom] = useState(15);
 
   const { reports, loading } = useFetchReports();
-
-  const successLocation = (position) => {
-    setLat(position.coords.latitude);
-    setLng(position.coords.longitude);
-  };
-
-  const errorLocation = () => {
-    setLat(0);
-    setLng(0);
-  };
 
   const [mapLoaded, setMapLoaded] = useState(false);
   const [semanticLocation, setSemanticLocation] = useState("");
@@ -38,15 +27,30 @@ const MapPage = () => {
     const map = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: zoom,
+      zoom: 15,
     });
 
-    navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-      enableHighAccuracy: true,
-    });
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+
+        console.log("done", lng, lat, position);
+        mapRef.current.flyTo({
+          center: [position.coords.longitude, position.coords.latitude],
+        });
+      },
+      () => {
+        setLat(0);
+        setLng(0);
+      },
+      {
+        enableHighAccuracy: false,
+      }
+    );
 
     const nav = new mapboxgl.NavigationControl();
+
     map.addControl(nav, "top-right");
 
     var directions = new MapboxDirections({
@@ -55,37 +59,37 @@ const MapPage = () => {
       },
       accessToken: mapboxgl.accessToken,
       profile: "mapbox/driving",
-      placeholderOrigin: semanticLocation,
+      // placeholderOrigin: semanticLocation,
     });
 
     map.addControl(directions, "top-left");
+
     map.on("data", () => {
       setMapLoaded(true);
     });
 
     map.on("load", async function () {
-      directions.setOrigin([lng, lat]);
-      const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?limit=1&types=place%2Cpostcode%2Caddress&access_token=${mapboxgl.accessToken}`;
-      await fetch(url)
-        .then((response) => {
-          return response.json();
-        })
-        .then((jsonResponse) => {
-          if (!jsonResponse.features[0]) {
-            setSemanticLocation("Choose starting location");
-          } else {
-            setSemanticLocation(jsonResponse.features[0].place_name);
-          }
-        });
+      // directions.setOrigin([lng, lat]);
+      // const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?limit=1&types=place%2Cpostcode%2Caddress&access_token=${mapboxgl.accessToken}`;
+      // await fetch(url)
+      //   .then((response) => {
+      //     return response.json();
+      //   })
+      //   .then((jsonResponse) => {
+      //     if (!jsonResponse.features[0]) {
+      //       setSemanticLocation("Choose starting location");
+      //     } else {
+      //       setSemanticLocation(jsonResponse.features[0].place_name);
+      //     }
+      //   });
     });
-    
 
     mapRef.current = map;
 
     return () => {
       map.off("data");
     };
-  }, [lat, lng, zoom, semanticLocation]);
+  }, []);
 
   useEffect(() => {
     if (loading || !mapLoaded) {
@@ -110,7 +114,7 @@ const MapPage = () => {
       style={{ height: "calc(100vh-120px)" }}
       height="calc(100vh - 128px)"
       ref={mapContainer}
-    ></Box>
+    />
   );
 };
 
